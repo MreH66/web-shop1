5003b67bafa61288486f35c28f971ad2add97c4d
MreH66 <110855455+MreH66@users.noreply.github.com>
2024-05-14 21:25:28 +0200

    Update fireBaseUtils.js

--- src/sing-in/fireBase/fireBaseUtils.js
+++ src/sing-in/fireBase/fireBaseUtils.js
@@ -10,16 +10,9 @@ import {
 import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAI_eheDed8CMl4_Gtl_9J3-wXBMhOme3k",
-  authDomain: "web-shop-fashion.firebaseapp.com",
-  projectId: "web-shop-fashion",
-  storageBucket: "web-shop-fashion.appspot.com",
-  messagingSenderId: "799153726306",
-  appId: "1:799153726306:web:91ec4b731b5a23e856023b",
-};
+const firebaseConfig = {};
 
-export const emailID = "eMOGiGlPfpb2NcTYXJb5r5aIDAx1"
+export const emailID = ""
 
 const app = initializeApp(firebaseConfig);
 
